ab3858b712c61c2cfd8db85d788a9c602018182d
FerSReD Official <136520073+FerSReD98@users.noreply.github.com>
2023-10-28 16:53:53 +0300

    fsociety.py

--- fsociety.py
+++ fsociety.py
@@ -1294,8 +1294,6 @@ def gravity():
     sites = bing_all_grabber(str(ip))
     gravityforms = check_gravityforms(sites)
     for ss in gravityforms:
-        print ss
-
     print('\n')
     print('[*] Found, ', len(gravityforms), ' gravityforms.')
 
